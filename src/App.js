af0444528f00fcfe2bcd41049ae7cb898192c10c
jordan819 <patryk.zaucha00@gmail.com>
2021-04-30 12:04:29 +0200

    navbar and NotFound added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Posts from "./components/posts";
+import NavBar from "./components/common/navbar";
+import Home from "./components/home";
+import NotFound from "./components/notFound";
+import { Redirect } from 'react-router';
 
 function App() {
-  return (
-    <main className="container">
-      <Posts/>
-    </main>
-  );
+ return (
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   component={Posts} />}
+                   />
+                   <Route path="/not-found" exact component={NotFound} />
+                   <Route path="/" exact component={Home}/>
+                    <Redirect to="/not-found" />
+
+               </Switch>
+           </div>
+       </div>
+     </div>
+ );
 }
 
 export default App;
